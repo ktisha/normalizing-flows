4b0fe4d7f63ba222fa8b4aea461e9babefe32ff1
Sergei Lebedev <superbobry@gmail.com>
2015-10-23 17:51:04 +0300

    Added CLI to NF-VAE on MNIST

--- experiments/nf_mnist.py
+++ experiments/nf_mnist.py
@@ -1,3 +1,4 @@
+import argparse
 import time
 import pickle
 
@@ -15,8 +16,7 @@ from .layers import GaussianNoiseLayer
 from .utils import mvn_log_logpdf, mvn_std_logpdf, iter_minibatches
 
 
-def build_model(batch_size, num_features, num_latent=100, num_hidden=500,
-                num_flows=2):
+def build_model(batch_size, num_features, num_latent, num_hidden, num_flows):
     net = {}
 
     # q(z|x)
@@ -54,15 +54,6 @@ def build_model(batch_size, num_features, num_latent=100, num_hidden=500,
     return net
 
 
-def elbo(X_var, x_mu_var, x_log_covar_var, z_var, z_mu_var, z_log_covar_var):
-    # L(x) = E_q(z|x)[log p(x|z) + log p(z) - log q(z|x)]
-    return (
-        mvn_log_logpdf(X_var, x_mu_var, x_log_covar_var)
-        + mvn_std_logpdf(z_var)
-        - mvn_log_logpdf(z_var, z_mu_var, z_log_covar_var)
-    ).mean()
-
-
 def elbo_nf(X_var, x_mu_var, x_log_covar_var, z_0_var, z_k_var, logdet_var):
     # L(x) = E_q(z|x)[log p(x|z) + log p(z) - log q(z|x)]
     return (
@@ -72,14 +63,15 @@ def elbo_nf(X_var, x_mu_var, x_log_covar_var, z_0_var, z_k_var, logdet_var):
     ).mean()
 
 
-def main(batch_size=500, n_epochs=500):
+def main(num_latent, num_hidden, num_flows, batch_size, num_epochs):
     print("Loading data...")
     X_train, y_train, X_val, y_val, X_test, y_test = load_mnist_dataset()
     num_features = X_train.shape[1]
 
     print("Building model and compiling functions...")
     X_var = T.matrix("X")
-    net = build_model(batch_size, num_features)
+    net = build_model(batch_size, num_features, num_latent, num_hidden,
+                      num_flows)
 
     vars = ["x_mu", "x_log_covar", "z", "z_k"]
     x_mu_var, x_log_covar_var, z_0_var, z_k_var, *logdet_var = get_output(
@@ -105,7 +97,7 @@ def main(batch_size=500, n_epochs=500):
     print("Starting training...")
     train_errs = []
     val_errs = []
-    for epoch in range(n_epochs):
+    for epoch in range(num_epochs):
         start_time = time.perf_counter()
 
         train_err, train_batches = 0, 0
@@ -120,22 +112,32 @@ def main(batch_size=500, n_epochs=500):
             val_batches += 1
 
         print("Epoch {} of {} took {:.3f}s".format(
-            epoch + 1, n_epochs, time.perf_counter() - start_time))
+            epoch + 1, num_epochs, time.perf_counter() - start_time))
         print("  training loss:\t\t{:.6f}".format(train_err / train_batches))
         print("  validation loss:\t\t{:.6f}".format(val_err / val_batches))
         train_errs.append(train_err)
         val_errs.append(val_err)
 
+    prefix = "nf_mnist_L{}_F{}".format(num_latent, num_flows)
     pd.DataFrame.from_dict({
         "train_err": train_errs,
         "val_err": val_errs
-    }).to_csv("./nf_mnist_err.csv", index=False)
+    }).to_csv(prefix + ".csv", index=False)
 
     all_param_values = get_all_param_values(
         concat([net["x_mu"], net["x_log_covar"]]))
-    with open("./nf_mnist.pickle", "wb") as handle:
+    with open(prefix + ".pickle", "wb") as handle:
         pickle.dump(all_param_values, handle)
 
 
 if __name__ == "__main__":
-    main(n_epochs=1)
+    parser = argparse.ArgumentParser(
+        description="Learn NF-VAE from MNIST data")
+    parser.add_argument("-L", dest="num_latent", type=int, default=100)
+    parser.add_argument("-H", dest="num_hidden", type=int, default=500)
+    parser.add_argument("-F", dest="num_flows", type=int, default=2)
+    parser.add_argument("-E", dest="num_epochs", type=int, default=1000)
+    parser.add_argument("-B", dest="batch_size", type=int, default=500)
+
+    args = parser.parse_args()
+    main(**vars(args))
