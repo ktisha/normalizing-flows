2cc044e7686dbfe452984806691537cf7267c360
Sergei Lebedev <superbobry@gmail.com>
2015-11-24 22:05:33 +0300

    Allowed varying the number of steps in ``manifold``

--- experiments/nf_mnist.py
+++ experiments/nf_mnist.py
@@ -171,24 +171,25 @@ def load_model(path):
     return net
 
 
-def plot_manifold(path):
+def plot_manifold(path, num_steps):
     net = load_model(path)
     z_var = T.matrix()
     decoder = theano.function([z_var], get_output(net["x_mu"], {net["z"]: z_var}))
 
     figure = plt.figure()
 
-    Z01 = np.linspace(-1, 1, num=16)
+    Z01 = np.linspace(-1, 1, num=num_steps)
     Zgrid = as_floatX(np.dstack(np.meshgrid(Z01, Z01)).reshape(-1, 2))
 
     for (i, z_i) in enumerate(Zgrid, 1):
-        figure.add_subplot(16, 16, i)
+        figure.add_subplot(num_subplots, num_subplots, i)
         image = decoder(np.array([z_i])).reshape((28, 28))
         plt.axis('off')
         plt.imshow(image, cmap=cm.Greys)
 
     plt.subplots_adjust(wspace=0, hspace=0)
-    plt.savefig(str(path.with_name(path.stem + "_manifold.png")))
+    plt.savefig(str(path.with_name(
+        "{}_manifold_{}.png".format(path.stem, num_steps))))
 
 
 def plot_sample(path, num_samples):
@@ -231,6 +232,7 @@ if __name__ == "__main__":
 
     manifold_parser = subparsers.add_parser("manifold")
     manifold_parser.add_argument("path", type=Path)
+    manifold_parser.add_argument("-N", dest="num_steps", type=int, default=16)
     manifold_parser.set_defaults(command=plot_manifold)
 
     sample_parser = subparsers.add_parser("sample")
