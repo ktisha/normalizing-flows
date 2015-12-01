8c929d80651aaa8fb3e7932a8c466bf93da5c3de
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2015-12-01 18:40:58 +0300

    removed limits for train/test plot

--- tomato/plot_utils.py
+++ tomato/plot_utils.py
@@ -70,7 +70,6 @@ def plot_errors(path):
     plot_name = str(path.with_name(path.stem + "_train_val_errors.png"))
     errors = np.genfromtxt(str(path), delimiter=',')
     fig, ax = plt.subplots()
-    ax.set_ylim([-100000, 20000])
     train_errors = errors[1:, 0]
     val_errors = errors[1:, 1]
 
