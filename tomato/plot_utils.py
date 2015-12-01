3c6bbee6051d20df81b5b473341be3246fb8a811
Sergei Lebedev <superbobry@gmail.com>
2015-12-01 13:31:49 +0300

    Hardcoded Bernoulli sampling in ``plot_sample``

--- tomato/plot_utils.py
+++ tomato/plot_utils.py
@@ -1,35 +1,31 @@
 import re
-import theano.tensor as T
-import theano
-from lasagne.layers import get_output
+
 import matplotlib.pyplot as plt
 import numpy as np
+import theano
+import theano.tensor as T
+from lasagne.layers import get_output
 from lasagne.utils import floatX as as_floatX
 from matplotlib import cm
 from matplotlib.gridspec import GridSpec
 
 
-def plot_manifold(path, load_model, bounds=[-4, 4], num_steps=32):
+def plot_manifold(path, load_model, bounds=(-4, 4), num_steps=32):
     net = load_model(path)
     z_var = T.matrix()
-    decoder = theano.function([z_var], get_output(net["x_mu"], {net["z"]: z_var}))
-
-    figure = plt.figure()
+    decoder = theano.function(
+        [z_var], get_output(net["x_mu"], {net["z"]: z_var}))
 
     Z01 = np.linspace(*bounds, num=num_steps)
     Zgrid = as_floatX(np.dstack(np.meshgrid(Z01, Z01)).reshape(-1, 2))
 
-    gs = GridSpec(num_steps, num_steps)
-    gs.update(wspace=0.1, hspace=0.1, left=0.1, right=0.4, bottom=0.1, top=0.9)
-    for (i, z_i) in enumerate(Zgrid, 0):
-        plt.subplot(gs[i])
-        image = decoder(np.array([z_i])).reshape((28, 20))
-        plt.imshow(image, cmap=cm.Greys_r)
-        plt.axis("off")
+    images = []
+    for z_i in Zgrid:
+        images.append(decoder(np.array([z_i])).reshape((28, 20)))
 
-    plt.savefig(str(path.with_name("{}_manifold_{}.png"
-                                   .format(path.stem, num_steps))),
-                bbox_inches="tight")
+    _plot_grid(
+        path.with_name("{}_manifold_{}.png".format(path.stem, num_steps)),
+        images)
 
 
 def plot_sample(path, load_model, num_samples, prefix):
@@ -40,26 +36,35 @@ def plot_sample(path, load_model, num_samples, prefix):
     z_covar = theano.function(
         [z_var], T.exp(get_output(net["x_log_covar"], {net["z"]: z_var})))
 
-    figure = plt.figure()
     [chunk] = re.findall(prefix + r"(\d+)_H(\d+)", str(path))
     num_latent, _ = map(int, chunk)
 
-    num_subplots = int(np.sqrt(num_samples))
-    z = as_floatX(np.random.normal(size=(num_samples, num_latent)))
-    for i, z_i in enumerate(z, 1):
+    images = []
+    for z_i in as_floatX(np.random.normal(size=(num_samples, num_latent))):
         mu = z_mu(np.array([z_i]))
-        covar = z_covar(np.array([z_i]))
-        x = np.random.normal(mu, covar)
-        figure.add_subplot(num_subplots, num_subplots, i)
-        plt.axis("off")
-        plt.imshow(x.reshape((28, 28)), cmap=cm.Greys)
+        # covar = z_covar(np.array([z_i]))
+        # images.append(np.random.normal(mu, covar).reshape((28, 20)))
+        images.append(np.random.binomial(1, mu).reshape(28, 20))
 
-    plt.subplots_adjust(wspace=0, hspace=0)
-    plt.savefig(str(path.with_name("{}_sample_{}.png".format(path.stem, num_samples))))
+    _plot_grid(
+        path.with_name("{}_sample_{}.png".format(path.stem, num_samples)),
+        images)
+
+
+def _plot_grid(path, images):
+    num_subplots = int(np.sqrt(len(images)))
+    gs = GridSpec(num_subplots, num_subplots)
+    gs.update(wspace=0.1, hspace=0.1, left=0.1, right=0.4, bottom=0.1, top=0.9)
+    for i, image in enumerate(images):
+        plt.subplot(gs[i])
+        plt.imshow(image, cmap=cm.Greys_r)
+        plt.axis("off")
+
+    plt.savefig(str(path), bbox_inches="tight")
 
 
 def plot_errors(path):
-    plot_name=str(path.with_name(path.stem + "_train_val_errors.png"))
+    plot_name = str(path.with_name(path.stem + "_train_val_errors.png"))
     errors = np.genfromtxt(str(path), delimiter=',')
     fig, ax = plt.subplots()
     ax.set_ylim([-100000, 20000])
