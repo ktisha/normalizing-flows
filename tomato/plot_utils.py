bfc6ac2e533b9e866714cd28ab0ea822797cdbd7
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2016-02-19 19:28:43 +0300

    unified plotting for mnist

--- tomato/plot_utils.py
+++ tomato/plot_utils.py
@@ -18,7 +18,9 @@ def plot_manifold(path, load_model, load_params, num_steps):
     for i, j in np.ndindex(num_steps - 1, num_steps - 1):
         z_ij = as_floatX([[stats.norm.ppf((i + 1) / num_steps),
                            stats.norm.ppf((j + 1) / num_steps)]])
-        images.append(x_mu(z_ij).reshape(28, -1))
+        mu = x_mu(z_ij)
+        mu = np.where(mu <= 0.5, as_floatX(0), as_floatX(1))
+        images.append(mu.reshape(28, -1))
 
     _plot_grid(
         path.with_name("{}_manifold_{}.png".format(path.stem, num_steps)),
@@ -29,8 +31,8 @@ def plot_sample(path, load_model, load_params, num_samples):
     p = load_params(str(path))
     net = load_model(path)
     z_var = T.matrix()
-    z_mu_function = get_output(net["x_mu"], {net["z"]: z_var}, deterministic=True)
-    x_mu = theano.function([z_var], z_mu_function)
+    x_mu_function = get_output(net["x_mu"], {net["z"]: z_var}, deterministic=True)
+    x_mu = theano.function([z_var], x_mu_function)
 
     Z = as_floatX(np.random.normal(size=(num_samples, p.num_latent)))
 
@@ -45,7 +47,8 @@ def plot_sample(path, load_model, load_params, num_samples):
     else:
         for z_i in Z:
             mu = x_mu(np.array([z_i]))
-            images.append(np.random.binomial(1, mu).reshape(28, -1))
+            mu = np.where(mu <= 0.5, as_floatX(0), as_floatX(1))
+            images.append(mu.reshape(28, -1))
 
     _plot_grid(
         path.with_name("{}_sample_{}.png".format(path.stem, num_samples)),
